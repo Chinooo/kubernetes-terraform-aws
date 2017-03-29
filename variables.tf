54229d541a8106e9d73b444fc5609f10c8399783
Jairo Lopez <jaloare@gmail.com>
2017-03-29 14:36:20 -0600

    FIX fmt and ADD outputs

--- variables.tf
+++ variables.tf
@@ -6,22 +6,35 @@ variable "region" {
   default = "us-west-2"
 }
 
-variable "cluster_name" {
-    default = "testing"
+variable "subnet_zone" {
+  default = "us-west-2a"
 }
 
+variable "cluster_name" {
+  default = "test"
+}
+
+// ami-7ac6491a :: ubuntu-xenial-16.04-amd64-hvm-ssd/ebs
 variable "ami" {
-    default = "ami-2709bd47"
+  default = "ami-7ac6491a"
 }
 
 variable "master_instance_type" {
-    default = "m4.xlarge"
+  default = "m4.xlarge"
 }
 
 variable "minion_instance_type" {
-    default = "m4.xlarge"
+  default = "m4.xlarge"
 }
 
 variable "api_secure_port" {
-    default = "443"
+  default = "443"
+}
+
+output "master_ip" {
+  value = "${aws_instance.master.public_ip}"
+}
+
+output "minion_ip" {
+  value = "${aws_instance.minion.public_ip}"
 }
